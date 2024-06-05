d016da750e45ed8c08f673ac700652c032aaeb07
chris <leosameric@gmail.com>
2024-06-05 18:01:14 +1000

    feat: Update read contract hook

---
+++ apps/bridge-ui/src/contract/hooks/useGetAllowance.ts
@@ -0,0 +1,24 @@
+'use client';
+
+import { useAccount } from 'wagmi';
+import { ERC20_TOKEN } from '@/contract/abi';
+import { useContractRead } from 'wagmi';
+
+export const useGetAllowance = ({
+  tokenAddress,
+  sender,
+}: {
+  tokenAddress: `0x${string}`;
+  sender: `0x${string}`;
+}) => {
+  const { address } = useAccount();
+  const { data: allowance } = useContractRead({
+    abi: ERC20_TOKEN,
+    address: tokenAddress,
+    functionName: 'allowance',
+    args: [address as `0x${string}`, sender],
+  });
+  return {
+    allowance,
+  };
+};
--- apps/bridge-ui/src/contract/hooks/useGetTokenBalance.ts
+++ apps/bridge-ui/src/contract/hooks/useGetTokenBalance.ts
@@ -1,32 +1,28 @@
 'use client';
 import { ERC20_TOKEN } from '@/contract/abi';
-import { useCallback } from 'react';
-import { useAccount } from 'wagmi';
-import { config } from '@/app/html';
-import { readContract } from 'wagmi/actions';
+import { useAccount } from '@bridge/wallet';
+import { useContractRead } from 'wagmi';
 
-export const useGetTokenBalance = () => {
+export const useGetTokenBalance = ({
+  tokenAddress,
+}: {
+  tokenAddress: `0x${string}`;
+}) => {
   const { address } = useAccount();
-  const getTokenBalance = useCallback(
-    async (tokenAddress: `0x${string}`) => {
-      if (!address || !tokenAddress) return;
-      try {
-        const balance = await readContract(config, {
-          address: tokenAddress,
-          abi: ERC20_TOKEN,
-          functionName: 'balanceOf',
-          args: [address as `0x${string}`],
-        });
-        return balance;
-      } catch (e) {
-        // eslint-disable-next-line no-console
-        console.log(e);
-      }
-    },
-    [address]
-  );
 
+  const {
+    data: balance,
+    isError,
+    error,
+  } = useContractRead({
+    abi: ERC20_TOKEN,
+    address: tokenAddress,
+    functionName: 'balanceOf',
+    args: [address as `0x${string}`],
+  });
+
+  console.log(isError, error);
   return {
-    getTokenBalance,
+    balance,
   };
 };
