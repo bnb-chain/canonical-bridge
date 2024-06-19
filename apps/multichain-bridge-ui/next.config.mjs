25447449516ba8e1737dff6708e46bd778d0db39
chris <leosameric@gmail.com>
2024-06-19 21:34:21 +1000

    chore: Get public next env

--- apps/multichain-bridge-ui/next.config.mjs
+++ apps/multichain-bridge-ui/next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+const getPublicEnv = (prefix) => {
+  const envs = process.env;
+  const res = {};
+
+  Object.keys(envs).forEach((key) => {
+    if (key.startsWith(prefix)) {
+      res[key] = envs[key];
+    }
+  });
+
+  return res;
+};
+
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    ...getPublicEnv('NEXT_PUBLIC_'),
+  },
 };
 
 export default nextConfig;
