d24bb4407941c9ad0d4a288375678b65d47091bc
aidencao <aiden.c@nodereal.io>
2024-09-26 18:07:47 +0800

    feat(canonical-bridge-widget): Fix native token tooltip

--- packages/canonical-bridge-widget/src/modules/transfer/components/TransferOverview/RouteInfo/TokenInfoTooltip.tsx
+++ packages/canonical-bridge-widget/src/modules/transfer/components/TransferOverview/RouteInfo/TokenInfoTooltip.tsx
@@ -11,6 +11,9 @@ import {
   LightMode,
   Portal,
 } from '@bnb-chain/space';
+import { useMemo } from 'react';
+
+import { isNativeToken } from '@/core/utils/address.ts';
 
 interface TokenTooltipProps {
   tokenLinkUrl: string;
@@ -26,6 +29,7 @@ export const TokenInfoTooltip = ({
   isReceiveArea,
 }: TokenTooltipProps) => {
   const theme = useTheme();
+  const nativeToken = useMemo(() => isNativeToken(tokenAddress), [tokenAddress]);
 
   return (
     <Flex display={'inline-block'} w={'auto'}>
@@ -43,29 +47,31 @@ export const TokenInfoTooltip = ({
               <PopoverBody px={'8px'} py={'7px'} onClick={(e) => e.stopPropagation()}>
                 <Box>
                   <Box color={theme.colors.light.text.primary} fontSize={'12px'} fontWeight={400}>
-                    Token address:
+                    {nativeToken ? 'Native token' : 'Token address:'}
                   </Box>
-                  <Box color={theme.colors.light.text.primary} fontSize={'12px'} fontWeight={700}>
-                    {tokenAddress && (
-                      <Link
-                        isExternal
-                        href={tokenLinkUrl}
-                        display="block"
-                        overflowWrap={'break-word'}
-                        pointerEvents={'all'}
-                        color="currentColor"
-                        _hover={
-                          tokenLinkUrl
-                            ? {
-                                color: theme.colors.light.modal.item.text.primary,
-                              }
-                            : undefined
-                        }
-                      >
-                        {tokenAddress}
-                      </Link>
-                    )}
-                  </Box>
+                  {!nativeToken && (
+                    <Box color={theme.colors.light.text.primary} fontSize={'12px'} fontWeight={700}>
+                      {tokenAddress && (
+                        <Link
+                          isExternal
+                          href={tokenLinkUrl}
+                          display="block"
+                          overflowWrap={'break-word'}
+                          pointerEvents={'all'}
+                          color="currentColor"
+                          _hover={
+                            tokenLinkUrl
+                              ? {
+                                  color: theme.colors.light.modal.item.text.primary,
+                                }
+                              : undefined
+                          }
+                        >
+                          {tokenAddress}
+                        </Link>
+                      )}
+                    </Box>
+                  )}
                 </Box>
               </PopoverBody>
             </PopoverContent>
