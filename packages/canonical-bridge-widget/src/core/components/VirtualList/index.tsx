741134fbf3b0fd87c6eb8a556dabdeff48919187
Wenty Li <105278450+wenty22@users.noreply.github.com>
2024-09-26 15:40:49 +0800

    fix: Add new key `ethereum:symbol` to get token price (#181)
    * feat(canonical-bridge-widget): Update tooltip styles
    
    * fix(canonical-bridge-widget): Fix profile styles
    
    * fix: Tooltip component
    
    * fix: Fix balance display issue
    
    * feat(canonical-bridge-widget): Fix ui issues
    
    * fix: Fix stargate allowed range
    
    * fix: Fix route display
    
    * feat: Handle loading gas fee error
    
    * fix: Fix fee loading icon issue
    
    * fix(canonical-bridge-widget): Fixed  virtual list cannot stop scrolling up on mobile
    
    * fix: Add new key `ethereum:symbol` to get token price
    
    * feat(canonical-bridge-widget): Fix tooltip position
    
    * feat(canonical-bridge-widget): Fix tooltip click events
    
    ---------
    
    Co-authored-by: chris <leosameric@gmail.com>
    Co-authored-by: aidencao <aiden.c@nodereal.io>

--- packages/canonical-bridge-widget/src/core/components/VirtualList/index.tsx
+++ packages/canonical-bridge-widget/src/core/components/VirtualList/index.tsx
@@ -1,4 +1,8 @@
+import { Flex } from '@bnb-chain/space';
+import { useMemo } from 'react';
 import { Virtuoso } from 'react-virtuoso';
+
+import { useResponsive } from '@/core/hooks/useResponsive';
 
 export interface VirtualListProps<T> {
   data: T[];
@@ -9,16 +13,39 @@ export interface VirtualListProps<T> {
 export function VirtualList<T>(props: VirtualListProps<T>) {
   const { data, children } = props;
 
+  const { isMobile } = useResponsive();
+
+  const scrollbarStyle = useMemo(() => {
+    if (isMobile) return undefined;
+    return {
+      '>div': {
+        '::-webkit-scrollbar': {
+          width: '8px',
+        },
+        '::-webkit-scrollbar-track': {
+          bg: 'transparent',
+        },
+        '::-webkit-scrollbar-thumb': {
+          h: '20px',
+          bg: 'rgba(0, 0, 0, 0.5)',
+          borderRadius: '99px',
+        },
+      },
+    };
+  }, [isMobile]);
+
   return (
-    <Virtuoso
-      style={{
-        width: '100%',
-        height: '100%',
-      }}
-      totalCount={data.length}
-      itemContent={(index) => {
-        return children(data[index], index);
-      }}
-    />
+    <Flex boxSize="100%" sx={scrollbarStyle}>
+      <Virtuoso
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+        totalCount={data.length}
+        itemContent={(index) => {
+          return children(data[index], index);
+        }}
+      />
+    </Flex>
   );
 }
