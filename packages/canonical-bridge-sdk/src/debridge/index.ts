353f0d226259fb2e92c84eed0d21b5c09d23914a
chris <leosameric@gmail.com>
2024-12-04 19:41:09 +1000

    test: Remove token address validation

--- packages/canonical-bridge-sdk/src/debridge/index.ts
+++ packages/canonical-bridge-sdk/src/debridge/index.ts
@@ -83,7 +83,7 @@ export class DeBridge {
     try {
       const deBridgeParams = {
         srcChainId: fromChainId,
-        srcChainTokenIn: fromTokenAddress,
+        srcChainTokenIn: '0xd5da8318cE7ca005E8F5285Db0e750CA9256586e',
         srcChainTokenInAmount: amount,
         dstChainId: toChainId,
         dstChainTokenOut: toTokenAddress,
