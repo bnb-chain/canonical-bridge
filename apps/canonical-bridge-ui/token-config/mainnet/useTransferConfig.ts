3aa946fecffbffae3891a94efc206293a170b226
chris <leosameric@gmail.com>
2024-12-12 16:31:07 +1000

    test: Test deploy

--- apps/canonical-bridge-ui/token-config/mainnet/useTransferConfig.ts
+++ apps/canonical-bridge-ui/token-config/mainnet/useTransferConfig.ts
@@ -26,8 +26,8 @@ export function useTransferConfig() {
 
       const transferConfig: ITransferConfig = {
         defaultSelectedInfo: {
-          fromChainId: 56,
-          toChainId: 42161,
+          fromChainId: 1,
+          toChainId: 56,
           tokenSymbol: 'USDT', // USDT
           amount: '',
         },
