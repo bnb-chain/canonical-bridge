3fd4ca7333eda2782c53aada23d14db08f9a4292
wenty22 <wenty.l@nodereal.io>
2024-12-23 17:05:46 +0800

    refactor: Import local packages from npm

--- apps/canonical-bridge-ui/next.config.js
+++ apps/canonical-bridge-ui/next.config.js
@@ -8,4 +8,5 @@ module.exports = {
   images: {
     unoptimized: true,
   },
+  transpilePackages: ['@bnb-chain/space'],
 };
--- packages/space/src/components/Toast/context.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-import { Status } from '../internal/Status';
-
-export const Context = createContext<{
-  status: Status;
-}>({ status: 'info' });
