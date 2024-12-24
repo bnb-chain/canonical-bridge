ef7a857ea580036631bac50db9567b03cdf78259
wenty22 <wenty.l@nodereal.io>
2024-12-24 15:46:42 +0800

    fix: Fix security issues

--- apps/canonical-bridge-ui/pages/_document.tsx
+++ apps/canonical-bridge-ui/pages/_document.tsx
@@ -10,16 +10,6 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head>
           <link rel="stylesheet" href={`${env.ASSET_PREFIX}/fonts/index.css`} />
-          <script src="https://unpkg.com/vconsole@latest/dist/vconsole.min.js" defer></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.onload = () => {
-                new window.VConsole();
-              }
-            `,
-            }}
-          />
           <style
             dangerouslySetInnerHTML={{
               __html: `
