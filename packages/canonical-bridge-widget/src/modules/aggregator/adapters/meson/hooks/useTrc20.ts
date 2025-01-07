0daeb9ce9fb1e7172e81ca4529177ae9dcc75467
chris <leosameric@gmail.com>
2025-01-07 12:56:26 +1000

    fix: Fix infinite loop

--- packages/canonical-bridge-widget/src/modules/aggregator/adapters/meson/hooks/useTrc20.ts
+++ packages/canonical-bridge-widget/src/modules/aggregator/adapters/meson/hooks/useTrc20.ts
@@ -1,4 +1,5 @@
 import { useWallet as useTronWallet } from '@tronweb3/tronwallet-adapter-react-hooks';
+import { useCallback } from 'react';
 
 import { useTronWeb } from '@/core/hooks/useTronWeb';
 
@@ -40,28 +41,34 @@ export const useTrc20 = () => {
     return res;
   };
 
-  const getTrc20Balance = async ({ trc20Address }: { trc20Address: string }) => {
-    if (!address || !tronWeb) return;
-    tronWeb.setAddress(address);
-    const contract = await tronWeb.contract().at(trc20Address);
-    const balance = await contract.methods.balanceOf(address).call();
+  const getTrc20Balance = useCallback(
+    async ({ trc20Address }: { trc20Address: string }) => {
+      if (!address || !tronWeb) return;
+      tronWeb.setAddress(address);
+      const contract = await tronWeb.contract().at(trc20Address);
+      const balance = await contract.methods.balanceOf(address).call();
 
-    return balance;
-  };
+      return balance;
+    },
+    [address, tronWeb],
+  );
 
-  const getTrc20Allowance = async ({
-    trc20Address,
-    tronBridgeAddress,
-  }: {
-    trc20Address: string;
-    tronBridgeAddress: string;
-  }) => {
-    if (!address || !tronWeb) return;
-    tronWeb.setAddress(address);
-    const contract = await tronWeb.contract().at(trc20Address);
-    const allowance = await contract.methods.allowance(address, tronBridgeAddress).call();
-    return allowance;
-  };
+  const getTrc20Allowance = useCallback(
+    async ({
+      trc20Address,
+      tronBridgeAddress,
+    }: {
+      trc20Address: string;
+      tronBridgeAddress: string;
+    }) => {
+      if (!address || !tronWeb) return;
+      tronWeb.setAddress(address);
+      const contract = await tronWeb.contract().at(trc20Address);
+      const allowance = await contract.methods.allowance(address, tronBridgeAddress).call();
+      return allowance;
+    },
+    [address, tronWeb],
+  );
 
   return { approveTrc20, getTrc20Balance, getTrc20Allowance };
 };
