ac148b76663cee84f5870403693b098c719242f9
wenty22 <wenty.l@nodereal.io>
2025-02-10 19:05:23 +0800

    feat: Add disclaimer

--- apps/canonical-bridge-ui/core/components/Layout/Header.tsx
+++ apps/canonical-bridge-ui/core/components/Layout/Header.tsx
@@ -8,19 +8,32 @@ export function Header(props: FlexProps) {
   const { colorMode } = useColorMode();
 
   return (
-    <Flex
-      as="header"
-      h={['64px', '64px', '64px', '80px']}
-      borderBottom={`1px solid ${theme.colors[colorMode].border['3']}`}
-      alignItems="center"
-      justifyContent="space-between"
-      px={['20px', '20px', '20px', '64px']}
-      bg={theme.colors[colorMode].layer[2].default}
-      zIndex={1}
-      {...props}
-    >
-      <LogoIcon />
-      <ConnectWalletButton />
-    </Flex>
+    <>
+      <Flex
+        as="header"
+        h={['64px', '64px', '64px', '80px']}
+        borderBottom={`1px solid ${theme.colors[colorMode].border['3']}`}
+        alignItems="center"
+        justifyContent="space-between"
+        px={['20px', '20px', '20px', '64px']}
+        bg={theme.colors[colorMode].layer[2].default}
+        zIndex={1}
+        {...props}
+      >
+        <LogoIcon />
+        <ConnectWalletButton />
+      </Flex>
+
+      <Flex
+        fontSize="16px"
+        px={['20px', '20px', '20px', '64px']}
+        py={'8px'}
+        justifyContent="center"
+        bg="#F8545F"
+      >
+        This demo is for testing purposes only and non-developers should not use it to send
+        transactions.
+      </Flex>
+    </>
   );
 }
