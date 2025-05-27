3ac01166fe8a57834b646fc99cd340005a4b3c76
aidencao <aiden.c@nodereal.io>
2025-05-27 16:11:53 +0800

    feat(canonical-bridge-widget): Update mayan fee label

--- packages/canonical-bridge-widget/src/modules/aggregator/adapters/mayan/hooks/useGetMayanFees.tsx
+++ packages/canonical-bridge-widget/src/modules/aggregator/adapters/mayan/hooks/useGetMayanFees.tsx
@@ -18,7 +18,7 @@ export const useGetMayanFees = () => {
       // solanaRelayerFee
       if (quote.solanaRelayerFee) {
         feeBreakdown.push({
-          label: 'Solana Relayer Fee',
+          label: 'Solana Relayer Fee:',
           value: `${formatNumber(Number(quote.solanaRelayerFee), 8)} ${
             quote.type === 'WH' ? selectedToken?.symbol : 'USDC'
           }`,
@@ -29,7 +29,7 @@ export const useGetMayanFees = () => {
       // redeemRelayerFee
       if (quote.redeemRelayerFee) {
         feeBreakdown.push({
-          label: 'Redeem Relayer Fee',
+          label: 'Redeem Relayer Fee:',
           value: `${formatNumber(Number(quote.redeemRelayerFee), 8)} ${
             quote.type === 'WH' ? selectedToken?.symbol : 'USDC'
           }`,
